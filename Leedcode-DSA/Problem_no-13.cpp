ee1fcfe4171d9ed90d8bcf2fd63285df40d6f7d0
sagar de <sagarde2004@gmail.com>
2025-10-07 21:46:14 +0530

    Add files via upload

---
+++ Leedcode-DSA/Problem_no-13.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+    
+class Solution {
+public:
+    int num(char c){
+        if(c=='I')
+        return 1;
+        else if(c=='V')
+        return 5;
+        else if(c=='X')
+        return 10;
+        else if(c=='L')
+        return 50;
+        else if(c=='C')
+        return 100;
+        else if(c=='D')
+        return 500;
+    
+        return 1000;
+
+    }
+    int romanToInt(string s) {
+        int sum =0,index=0 ;
+        while(index<s.size()-1){
+            if(num(s[index])<num(s[index + 1]))
+            sum-=num(s[index]);
+
+            else
+            sum+=num(s[index]);
+        
+            index++;
+        }
+        sum+=num(s[s.size()-1]);
+        return sum;
+    }
+};
+int main(){
+    // leedcode problem--->13
+    // https://leetcode.com/problems/roman-to-integer/
+    return 0;
+}
